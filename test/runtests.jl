a40c7656d063fe020119ba1881e14f16644e19ea
Oliver Schulz <oschulz@mpp.mpg.de>
2021-10-11 23:37:55 +0200

    Use import instead of using in runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,18 +1,18 @@
 # This file is a part of ChangesOfVariables.jl, licensed under the MIT License (MIT).
 
-using ChangesOfVariables
-using Documenter
-using Test
+import Test
+import ChangesOfVariables
+import Documenter
 
 Test.@testset "Package ChangesOfVariables" begin
     include("test_with_ladj.jl")
 
     # doctests
-    DocMeta.setdocmeta!(
+    Documenter.DocMeta.setdocmeta!(
         ChangesOfVariables,
         :DocTestSetup,
         :(using ChangesOfVariables);
         recursive=true,
     )
-    doctest(ChangesOfVariables)
+    Documenter.doctest(ChangesOfVariables)
 end # testset
